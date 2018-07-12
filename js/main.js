c99993a56d3a1a8c00e61d466fd1c69bfa29ab31
Mocrotronics <aouali@gmail.com>
2018-07-12 23:11:14 +0200

    !

--- js/main.js
+++ js/main.js
@@ -3,8 +3,18 @@ document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
 function saveBookmark(e) {
     // get form values
-    var siteName = document.getElementById('siteName');
-    console.log(siteName);
+    var siteName = document.getElementById('siteName').value;
+    var siteUrl = document.getElementById('siteUrl').value;
+
+    var bookMark = {
+        name: siteName,
+        url: siteUrl
+    }
+
+    // local storage text
+    // parse json into string and return it back into json
+    localStorage.setItem('test', 'Hello world');
+
     // prevent default from submitting
     e.preventDefault();
 }
